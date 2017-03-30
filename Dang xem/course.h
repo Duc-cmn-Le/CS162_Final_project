2fc1c0912bdacad705ceade836bf430e928a71ef
Duc-cmn-Le <ldcduc@apcs.vn>
2017-03-30 13:07:46 +0700

    Update new folder

---
+++ Dang xem/course.h
@@ -0,0 +1,25 @@
+#ifndef _COURSE_H_
+#define _COURSE_H_
+
+struct Date
+{
+    int d, m, y;
+};
+
+struct Time
+{
+    int s, m, h;
+};
+
+enum Week = {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
+
+class course
+{
+    string coureCode, Year, courseName, lecturerName;
+    Date startAt, endAt;
+    Time From, To;
+    Week dateOfWeek;
+    int Semester;
+} ;
+
+#endif // _COURSE_H_
---
+++ Dang xem/presence.h
@@ -0,0 +1,17 @@
+// Nam
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class presence
+{
+protected:
+	string course_code;
+	string student_id;
+	int year;
+	int semeter;
+	int week;
+public:
+	void check_in (pair <string, string> primary_key, int week, int year, int semeter);
+}
