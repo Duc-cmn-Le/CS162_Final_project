be67b342313995ff409db93f1f9ca6be32190864
Duc-cmn-Le <ldcduc@apcs.vn>
2017-03-30 14:37:51 +0700

    Update CSV helper

---
+++ Area 96/main.cpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "CSVhelper.h"
+#include "UI.h"
+#include "SMS.h"
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
---
+++ CSV helper/CSVhelper.h
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <cmath>
+
+#include "user.h"
+#include "course.h"
+#include "score.h"
+#include "presence.h"
+
+using namespace std;
+
+User Get_details(string, );
+Course Get_details(string, );
+Presence Get_details(string, );
+Score Get_details(string, );
