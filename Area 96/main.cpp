ce2c5cbfa88c85fbf67a20e5f54d32f179230d50
Duc-cmn-Le <ldcduc@apcs.vn>
2017-04-02 19:12:16 +0700

    Update user.h user.cpp

--- Area 96/main.cpp
+++ Area 96/main.cpp
@@ -1,11 +1,15 @@
-#pragma once
-#include "CSVhelper.h"
-#include "UI.h"
-#include "SMS.h"
+//#include "CSVhelper.h"
+//#include "UI.h"
+//#include "SMS.h"
+#include "user.h"
 
 using namespace std;
 
 int main() {
-
+    Linkedlist a;
+    int n;
+    cin >> n;
+    for (;n;n--) a.Add_student_to_last();
+    a.Print_list();
     return 0;
 }
