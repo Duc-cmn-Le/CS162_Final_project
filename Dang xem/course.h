118d7b63d1b376368d741511466569d26eaa708b
hamaitrinh <hhmtrinh@apcs.vn>
2017-04-05 17:01:59 +0700

    Update course.h

--- Dang xem/course.h
+++ Dang xem/course.h
@@ -1,6 +1,10 @@
 #ifndef _COURSE_H_
 #define _COURSE_H_
 
+#include <iostream>
+#include <cstring>
+using namespace std;
+
 struct Date
 {
     int d, m, y;
@@ -11,15 +15,39 @@ struct Time
     int s, m, h;
 };
 
-enum Week = {monday, tuesday, wednesday, thursday, friday, saturday, sunday};
-
 class Course
 {
-    string coure_code, year, course_name, lecturer_name;
+    public:
+    string course_code, year, course_name, lecturer_name, date_of_week;
     Date start_at, end_at;
     Time from, to;
-    Week date_of_week;
     int semester;
+    void Input();
+    void Print();
+    void Print_one_line();
 } ;
+
+class Node
+{
+    public:
+    Course data;
+    Node* next;
+    Node(Course x);
+};
+
+class Course_list
+{
+    Node* head;
+public:
+    Course_list() {head = NULL;};
+    ~Course_list();
+    void Add_tail(Course x);
+    void Add_a_course();
+    void Delete_node(string s);
+    void Delete_a_course();
+    void Print_list();
+    void Print_list_one_line();
+    void Make_empty();
+};
 
 #endif
