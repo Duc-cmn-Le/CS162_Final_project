f58f1c6bb5f82c0c6f711ffbe86708750d25b0e4
giabao4498 <giabao441998@gmail.com>
2017-04-08 17:48:25 +0700

    Add files via upload

--- Gia Bao/Student_management_service.h
+++ Gia Bao/Student_management_service.h
@@ -2,6 +2,7 @@
 #define _SMS_H_
 #include "user.h"
 #include "course.h"
+#include "score.h"
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -12,4 +13,6 @@ public:
     void Export_scores_of_student(const string username);
     void Export_scores_of_course(const string course_id);
 };
+int to_int(const string);
+double to_double(const string);
 #endif
