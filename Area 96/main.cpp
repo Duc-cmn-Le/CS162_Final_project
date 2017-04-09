16c9077c495bd68728a6c16111cd4b894f1b9b4f
Duc-cmn-Le <ldcduc@apcs.vn>
2017-04-09 18:26:50 +0700

    Hash

--- Area 96/main.cpp
+++ Area 96/main.cpp
@@ -9,10 +9,10 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     Student_management_service Service;
-//    Service.Export_scores_of_student("1651044",cout);
+    Service.Export_scores_of_student("1651044",cout);
 //    Service.Export_scores_of_course("CS161",cout);
-    Service.Print_present("MTH251",cout);
-    Service.Print_absent("MTH251",cout);
+//    Service.Print_present("MTH251",cout);
+//    Service.Print_absent("MTH251",cout);
 //    Service.Import_student_list();
 //    Service.Import_score();
 //    Service.Import_course();
