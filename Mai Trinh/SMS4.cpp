901f82dbf354ef5e98d6a39661dbfb1eb05eb2a1
hhmtity <hhmtity@gmail.com>
2017-04-24 16:02:49 +0700

    Add files via upload

--- Mai Trinh/SMS4.cpp
+++ Mai Trinh/SMS4.cpp
@@ -1,4 +1,3 @@
-
 // Assign an existing class to a course
 void Student_management_service::Assign_one_class()
 {
@@ -17,6 +16,39 @@ void Student_management_service::Assign_one_class()
         cin >> year;
         cout << "Enter semester\n";
         cin >> semester;
-
+        filebuf fb, fbnew;
+        fb.open ("course_student.txt",ios::in);
+        fbnew.open ("course_student_new.txt",ios::out);
+        istream fin(&fb);
+        ostream fnew(&fbnew);
+        string S, s;
+        int c;
+        while (getline(fin,S)) {
+            c = Next_token(S,s);
+            fnew << s << ",";
+            if (s != course_code) {
+                fnew << S << endl;
+                continue;
+            }
+            c = Next_token(S,s);
+            fnew << s << ",";
+            if (s != year) {
+                fnew << S << endl;
+                continue;
+            }
+            c = Next_token(S,s);
+            fnew << s;
+            int tmp = 0;
+            for (string::iterator i=s.begin();i!=s.end();++i)
+                tmp = tmp*10 + *i - '0';
+            if (tmp != semester) {
+                fnew << "," << S << endl;
+                continue;
+            }
+            fnew << "," << tmp << endl;
+        }
+        fb.close();
+        fbnew.close();
+        system("mv course_student_new.txt course_student.txt");
     }
 }
