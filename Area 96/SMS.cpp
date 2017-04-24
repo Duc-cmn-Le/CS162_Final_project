79aba4cfe73de64c33c353e3ef96fb187acb898c
Duc-cmn-Le <ldcduc@apcs.vn>
2017-04-24 15:41:31 +0700

    Coding assign one class to a course

--- Area 96/SMS.cpp
+++ Area 96/SMS.cpp
@@ -318,6 +318,28 @@ void Student_management_service::Assign_one_student()
     return;
 }
 
+// Assign an existing class to a course
+void Student_management_service::Assign_one_class()
+{
+    string tmp, class_code;
+    cout << "Enter class code\n";
+    cin >> class_code;
+    Student_list a;
+    a.List_all_student_of_class(class_code,tmp;
+    if (a.Is_empty())
+        cout << "There is no student in this class " << class_code << '\n';
+    else {
+        string course_code, year; int semester;
+        cout << "Enter course code\n";
+        cin >> course_code;
+        cout << "Enter academic year\n";
+        cin >> year;
+        cout << "Enter semester\n";
+        cin >> semester;
+
+    }
+}
+
 // Show list of student in class - Cong Duc
 void Student_management_service::Show_list_of_student_in_class() 
 {
